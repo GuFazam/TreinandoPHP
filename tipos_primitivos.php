b3f10c73f3a72414f294046052bd226f89836faf
Gustavo H. Fazam <112525498+GuFazam@users.noreply.github.com>
2023-06-14 15:52:17 -0300

    tipos primitivos

---
+++ tipos_primitivos.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos</title>
+</head>
+<body>
+    <h1>tipos primitivos</h1>
+    <?php 
+    //0x é hexadecimal, 0b é binario e 0 é octal
+
+    /*
+    $v = "Gustavo";
+    var_dump($v); // var_dump() serve para imprimir na tela otipo da variavel
+
+    $num = "950";
+    var_dump($num);
+
+    $casado = true;
+    var_dump($casado);
+    print "O valor para casado é $casado";
+
+    $vetor = [6, 2.5, "Maria", 3, false];
+    $vardump($vetor) ;
+    */
+    ?>
+</body>
+</html>
